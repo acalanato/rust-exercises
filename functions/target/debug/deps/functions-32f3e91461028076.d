add858b7e27013f6bec8f9cd9dec8b7579fa6e69
acalanato <vagner.assistencia@gmail.com>
2024-05-11 11:06:33 -0300

    Transitional files

---
+++ functions/target/debug/deps/functions-32f3e91461028076.d
@@ -0,0 +1,5 @@
+/home/vagner/rust-exercises/functions/target/debug/deps/libfunctions-32f3e91461028076.rmeta: src/main.rs
+
+/home/vagner/rust-exercises/functions/target/debug/deps/functions-32f3e91461028076.d: src/main.rs
+
+src/main.rs:
