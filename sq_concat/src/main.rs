d4453dcbd887dde89b45841afc535ebe300fc4d8
acalanato <vagner.assistencia@gmail.com>
2024-05-22 12:55:02 -0300

    Trying with math

--- sq_concat/src/main.rs
+++ sq_concat/src/main.rs
@@ -4,12 +4,31 @@ fn main() {
     println!("{x:?}");
 }
 
+
+fn square_digits(num: u64) -> u64 {
+    let mut n = num;
+    let mut i = 0;
+    let mut _out = 0;
+    loop {
+	n = n / 10;
+	_out = n * n * 10 * i;
+	i += 1;
+	if n == 0 {break}
+    }
+    return _out;
+}
+//    num.next_power_of_two()
+//    return num;
+
+
+/*
 fn square_digits(num: u64) -> u64 {
     let binding = num.to_string();
     let s = binding.chars().filter_map(|x| x.to_digit(10));
-    let mut out = Vec::new();
+    let mut sq = Vec::new();
     for x in s.into_iter() {
-        out.push(x * x);
+        sq.push(x * x);
     };
     return out;
 }
+*/
---
+++ sq_concat/target/debug/deps/sq_concat-b2c7a8115c098048.d
@@ -0,0 +1,5 @@
+/home/vagner/rust-exercises/sq_concat/target/debug/deps/sq_concat-b2c7a8115c098048: src/main.rs
+
+/home/vagner/rust-exercises/sq_concat/target/debug/deps/sq_concat-b2c7a8115c098048.d: src/main.rs
+
+src/main.rs:
