8f29cc1fcbc1754b1f42cae58f0102e93fe3350f
acalanato <vagner.assistencia@gmail.com>
2024-05-25 18:42:02 -0300

    Done

--- sheep/target/debug/deps/sheep-69fb3edd8cdc4b6f.d
+++
@@ -1,5 +0,0 @@
-/home/vagner/rust-exercises/sheep/target/debug/deps/libsheep-69fb3edd8cdc4b6f.rmeta: src/main.rs
-
-/home/vagner/rust-exercises/sheep/target/debug/deps/sheep-69fb3edd8cdc4b6f.d: src/main.rs
-
-src/main.rs:
