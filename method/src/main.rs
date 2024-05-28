5a8e7c34d1184487821e1756532d8209cdb6d8ad
acalanato <vagner.assistencia@gmail.com>
2024-05-27 22:15:50 -0300

    Assossiate functions and bellow

--- method/src/main.rs
+++ method/src/main.rs
@@ -9,26 +9,41 @@ impl Rectangle {
 	self.width * self.height
     }
     fn width(&self) -> bool {
-        self.width() > 0
+        self.width > 0
+    }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
     }
 }
-
 
 fn main() {
     let rect1 = Rectangle {
 	width: 30,
 	height: 50,
     };
+        let rect2 = Rectangle {
+	width: 10,
+	height: 40,
+    };
+        let rect3 = Rectangle {
+	width: 60,
+	height: 45,
+    };
+    
 
     println!(
 	"The area of the rectangle is {} square pixels.",
 	rect1.area()
     );
 
-    let test = |rect1: Rectangle| -> u32 {rect1.width * rect1.height};
-    println!("Using the anonymous function syntax to calculate: {}", test(rect1));
+    let test = |rect1: &Rectangle| -> u32 {rect1.width * rect1.height};
+    println!("Using the anonymous function syntax to calculate: {}", test(&rect1));
     
     if rect1.width() {
         println!("The rectangle has a nonzero width; it is {}", rect1.width);
     }
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect3))
+
 }
