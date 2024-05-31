16e46de5eefa7245db154754fc7aa0ca60da891b
acalanato <vagner.assistencia@gmail.com>
2024-05-30 22:35:47 -0300

    Cleaning make files

---
+++ method/target/debug/deps/method-b4bf418d935372e6.d
@@ -0,0 +1,5 @@
+/home/vagner/rust-exercises/method/target/debug/deps/libmethod-b4bf418d935372e6.rmeta: src/main.rs
+
+/home/vagner/rust-exercises/method/target/debug/deps/method-b4bf418d935372e6.d: src/main.rs
+
+src/main.rs:
---
+++ method/target/debug/deps/method-ba468eac8e3402d0.d
@@ -0,0 +1,5 @@
+/home/vagner/rust-exercises/method/target/debug/deps/libmethod-ba468eac8e3402d0.rmeta: src/main.rs
+
+/home/vagner/rust-exercises/method/target/debug/deps/method-ba468eac8e3402d0.d: src/main.rs
+
+src/main.rs:
