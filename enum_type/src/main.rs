31178a5d2ad79d72b7b02e413d7c2307f63630e1
acalanato <vagner.assistencia@gmail.com>
2024-05-30 23:07:35 -0300

    Done

--- enum_type/src/main.rs
+++ enum_type/src/main.rs
@@ -131,5 +131,19 @@ One quarter:\t{}\n",
 
     );
     let cidade = estado_cidade(EstadoBr::SãoPaulo(CidadeBr::Atibaia));
-    println!("{}", cidade)
+    println!("{}", cidade);
+
+    //match with Option<T>
+    fn plus_one(x: Option<i32>) -> Option<i32> {
+        match x {
+            None => None,
+            Some(i) => Some(i + 1),
+        }
+    }
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+    println!("Five: {:?}\nsix: {:?}\nNone {:?}", five, six, none)
 }
+
