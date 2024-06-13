946e78b9e4829c0f9cbdd195a34581313642aff1
acalanato <vagner.assistencia@gmail.com>
2024-06-13 18:51:57 -0300

    Clean

--- dbus-vlc/src/main.rs
+++ dbus-vlc/src/main.rs
@@ -4,13 +4,13 @@ fn main() {
     use dbus::blocking::Connection;
     use dbus::message::SignalArgs;
     use dbus::blocking::stdintf::org_freedesktop_dbus::PropertiesPropertiesChanged as PG;
+    use dbus::blocking::stdintf::org_freedesktop_dbus::EmitsChangedSignal as ES;
     use std::time::Duration;
 
     let c = Connection::new_session().unwrap();
 
     let a = PG::match_rule(None, None); // rule out those args
 
-    #[allow(unused_must_use)]
     c.add_match(a, |ir: PG, _, _| {
         println!("{:?}", ir.changed_properties); //remember to remove debug
         true
