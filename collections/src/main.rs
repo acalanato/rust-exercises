25b2420e7aa2c0c27d3cca2ba1b91c20fcf572bd
acalanato <vagner.assistencia@gmail.com>
2024-06-14 20:48:38 -0300

    Need more bytes

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,5 +1,10 @@
+
 fn main() {
-    let v = vec![1, 2, 3, 4, 5];
+    let mut v = vec![1, 2, 3, 4, 5];
+    
+    let mut e = (6..=8).collect();
+
+    v.append(&mut e);
 
     let third: &i32 = &v[2];
     println!("The third element is {third}");
@@ -9,4 +14,5 @@ fn main() {
         Some(third) => println!("The third element is {third}"),
         None => println!("There is no third element."),
     }
+    println!("{:?}", v)
 }
