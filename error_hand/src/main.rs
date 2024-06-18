dab355845b2a029277bc72d6294b450707e96408
acalanato <vagner.assistencia@gmail.com>
2024-06-17 22:21:09 -0300

    Done

--- error_hand/src/main.rs
+++ error_hand/src/main.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
-use std::{error, fs, io};
-use std::io::{read_to_string, ErrorKind, Read};
+use std::{fs, io};
+use std::io::{ErrorKind, Read};
+use std::error::Error;
 
 fn read_from_f () -> String {
     let file_path = "my_file.txt";
@@ -23,7 +24,7 @@ fn read_from_f () -> String {
     return "You must mess up real bad to see this message".to_string();
 }
 
-fn read_file_error() -> Result<String, io::Error> {
+fn _read_file_error() -> Result<String, io::Error> {
     let username_file_result = File::open("not_present.txt");
 
     let mut username_file = match username_file_result {
@@ -36,6 +37,24 @@ fn read_file_error() -> Result<String, io::Error> {
         Ok(_) => Ok(username),
         Err(e) => Err(e),
     }
+}
+
+fn _read_username_from_file() -> Result<String, io::Error> {
+    let mut username_file = File::open("not_here.txt")?;
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
+    Ok(username)
+}
+
+fn _shorter_error() -> Result<String, io::Error> {
+
+    let mut username = String::new();
+    File::open("not_here_too.txt")?.read_to_string(&mut username)?;
+    Ok(username)
+}
+
+fn _shortest() -> Result<String, io::Error> {
+    fs::read_to_string("not_here")
 }
 
 /* < Doesn't work for reading to string >
@@ -53,14 +72,17 @@ fn read_f_too () -> String {
     return "".to_string()
 }
 */
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     //    panic!("crash with success!");
     //    let v = vec![3, 2, 1];
     //    v[10];
     let s = read_from_f();
     println!("{}", s);
 
-    let greeting_file = File::open("missing_file.txt")
+    let _greeting_file = File::open("missing_file.txt")
         .expect("file not inclued in project");
+
+    let _greeting = File::open("Hell_no.txt")?;
+    Ok(())
     //println!("add RUST_BACKTRACE=1 to env in order to backtrace");
 }
