b36d621bcdd661d5abeefedf0c306eaee44df598
acalanato <vagner.assistencia@gmail.com>
2024-06-18 22:04:12 -0300

    Done

--- gen_data/src/main.rs
+++ gen_data/src/main.rs
@@ -1,4 +1,3 @@
-//resume from listing 10-9
 
 fn largest<T: std::cmp::PartialOrd>(list: &[T]) ->&T {
     let mut largest = &list[0];
@@ -30,6 +29,26 @@ struct Point3D<A, B, C> {
     z: C,
 }
 
+struct Point2D<T> {
+    x: T,
+    y: T,
+}
+
+struct Point<X1, Y1> {
+    x: X1,
+    y: Y1,
+}
+
+#[allow(dead_code)]
+impl<X1, Y1> Point<X1, Y1> {
+    fn mixup<X2, Y2>(self, other: Point<X2, Y2>) -> Point<X1, Y2> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
 #[allow(dead_code)]
 impl<A, B, C> Point3D<A, B, C> {
     fn x(&self) -> &A {
@@ -37,10 +56,23 @@ impl<A, B, C> Point3D<A, B, C> {
     }
 }
 
+#[allow(dead_code)]
+impl Point2D<f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+
 fn main() {
     let _integer3d = Point3D {x: 5, y: 10, z: 2};
     let _float3d = Point3D {x: 1.0, y: 2.0, z: 5.0};
     let n_list = vec![10, 20, 2, 45, 80, 100];
     println!("{}", largest(&n_list));
-//    println!("Hello, world!");
+    let p1 = Point { x: 5, y: 10.5 };
+    let p2 = Point { x: "hold your horses", y: 'o' };
+
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
 }
