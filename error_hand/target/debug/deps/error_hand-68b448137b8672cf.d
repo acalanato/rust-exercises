a3fe6173aa638d8a306bf786d6a54f2c8f82597a
acalanato <vagner.assistencia@gmail.com>
2024-06-20 17:29:57 -0300

    Done

--- error_hand/target/debug/deps/error_hand-68b448137b8672cf.d
+++
@@ -1,5 +0,0 @@
-/home/vagner/repo/rust-exercises/error_hand/target/debug/deps/liberror_hand-68b448137b8672cf.rmeta: src/main.rs
-
-/home/vagner/repo/rust-exercises/error_hand/target/debug/deps/error_hand-68b448137b8672cf.d: src/main.rs
-
-src/main.rs:
--- error_hand/target/debug/deps/error_hand-d65cc87bf2c35383.d
+++
@@ -1,5 +0,0 @@
-/home/vagner/repo/rust-exercises/error_hand/target/debug/deps/liberror_hand-d65cc87bf2c35383.rmeta: src/main.rs
-
-/home/vagner/repo/rust-exercises/error_hand/target/debug/deps/error_hand-d65cc87bf2c35383.d: src/main.rs
-
-src/main.rs:
