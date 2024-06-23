629ed4b2ec0da54b7327ff760900b58098660e35
acalanato <vagner.assistencia@gmail.com>
2024-06-23 16:49:26 -0300

    Done

--- traits/target/debug/deps/traits-eab045d0104ebbab.d
+++
@@ -1,5 +0,0 @@
-/home/vagner/repo/rust-exercises/traits/target/debug/deps/libtraits-eab045d0104ebbab.rmeta: src/main.rs
-
-/home/vagner/repo/rust-exercises/traits/target/debug/deps/traits-eab045d0104ebbab.d: src/main.rs
-
-src/main.rs:
--- traits/target/debug/deps/traits-f3f73c1cb491db7f.d
+++
@@ -1,5 +0,0 @@
-/home/vagner/repo/rust-exercises/traits/target/debug/deps/libtraits-f3f73c1cb491db7f.rmeta: src/main.rs
-
-/home/vagner/repo/rust-exercises/traits/target/debug/deps/traits-f3f73c1cb491db7f.d: src/main.rs
-
-src/main.rs:
