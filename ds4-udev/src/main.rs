0732891eb29828864379925beda9cfaabf555bea
acalanato <vagner.assistencia@gmail.com>
2024-07-02 22:21:21 -0300

    Read test

--- ds4-udev/src/main.rs
+++ ds4-udev/src/main.rs
@@ -1,3 +1,12 @@
+use std::fs::read;
+
 fn main() {
-    println!("Hello, world!");
+    struct Js_event {
+        time: u32,
+        value: u16,
+        t: u8,
+        key: u8,
+    }
+    let file = read("/dev/input/js0");
+    println!("{:?}", file);
 }
