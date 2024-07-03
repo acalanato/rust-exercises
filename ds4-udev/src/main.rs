70e76daf6dd3be068a10d10b07de2346d86467cb
acalanato <vagner.assistencia@gmail.com>
2024-07-03 15:45:48 -0300

    Bk only

--- ds4-udev/src/main.rs
+++ ds4-udev/src/main.rs
@@ -1,12 +1,29 @@
-use std::fs::read;
+// follow from https://doc.rust-lang.org/std/io/
+use std::{fs::File, io::{self, Read}};
 
-fn main() {
-    struct Js_event {
+    struct JsEvent {
         time: u32,
         value: u16,
         t: u8,
         key: u8,
     }
-    let file = read("/dev/input/js0");
+
+impl Read for JsEvent {
+    fn read_exact(&mut self, buf: &mut [u8]) -> io::Result<()> {
+        self.read(buf)?;
+    }
+}
+
+fn main() {
+
+    
+    let mut js0 = File::open("/dev/input/js0")?;
+//        .ok()
+//        .expect("Can't find joystick");
+
+    let JsEvent {
+        time: js0.
+    }
+    
     println!("{:?}", file);
 }
