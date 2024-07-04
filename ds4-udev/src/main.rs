76cbad3e95a270664158486596b1c2bf74b7c199
acalanato <vagner.assistencia@gmail.com>
2024-07-04 13:41:05 -0300

    Bloking read?

--- ds4-udev/src/main.rs
+++ ds4-udev/src/main.rs
@@ -1,29 +1,39 @@
 // follow from https://doc.rust-lang.org/std/io/
-use std::{fs::File, io::{self, Read}};
+use std::fs;
 
-    struct JsEvent {
-        time: u32,
-        value: u16,
-        t: u8,
-        key: u8,
-    }
-
-impl Read for JsEvent {
-    fn read_exact(&mut self, buf: &mut [u8]) -> io::Result<()> {
-        self.read(buf)?;
-    }
+#[allow(dead_code)]
+struct JsEvent {
+    time: u32,
+    value: u16,
+    t: u8,
+    key: u8,
 }
+
+#[allow(dead_code)]
+struct InputEvent {
+        timeval: f32,
+        t: u8,
+        code: u8,
+        value: u32,
+}
+
+
+//impl Read for JsEvent {
+//    fn read_exact(&mut self, buf: &mut [u8]) -> io::Result<()> {
+//        self.read(buf)?;
+//    }
+//}
 
 fn main() {
 
+    let _default = "/dev/input/mouse0"; //funfa
+    let _udev_ = "/dev/input/event17"; //existe, mas nem encontra
+    let _test = "/home/vagner/out.txt"; //funfa com qq arquivo
     
-    let mut js0 = File::open("/dev/input/js0")?;
-//        .ok()
-//        .expect("Can't find joystick");
+    let js0: Vec<u8> = fs::read(_default)
+        .ok()
+        .expect("Can't find joystick");
 
-    let JsEvent {
-        time: js0.
-    }
     
-    println!("{:?}", file);
+    println!("{:?}", js0);
 }
