4b66f3407739f075d9da9d47664b6a0ea41dca68
acalanato <vagner.assistencia@gmail.com>
2024-07-12 19:22:11 -0300

    Main example

--- mongor/src/main.rs
+++ mongor/src/main.rs
@@ -1,7 +1,25 @@
-use std::io;
+use std::env;
+use std::error::Error;
 use tokio;
-use mongodb::{self, action::{ListDatabases, ListNames}, bson::Document, Client};
+use mongodb::{self, action::ListDatabases, options, Client};
 
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+   let client_uri =
+      env::var("MONGODB_URI").expect("You must set the MONGODB_URI environment var!");
+
+   let options =
+      ClientOptions::parse_with_resolver_config(&client_uri, ResolverConfig::cloudflare())
+         .await?;
+   let client = Client::with_options(options)?;
+
+   println!("Databases:");
+   for name in client.list_database_names(None, None).await? {
+      println!("- {}", name);
+   }
+    Ok(())
+}
 
 async fn list<'a>() -> ListDatabases<'a> {
     let client = Client::with_uri_str("").await
