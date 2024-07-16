f58beaa94c880e7031f575bcec3614a98f0ace16
acalanato <vagner.assistencia@gmail.com>
2024-07-15 21:31:01 -0300

    Tests

--- words/src/main.rs
+++ words/src/main.rs
@@ -1,8 +1,10 @@
 use std::collections::HashMap;
 use std::fs::read_to_string;
-use rand::Rng;
+use rand::{Rng, SeedableRng, prelude};
+
 
 const FILE: &str = "file.txt";
+const N: usize = 64;
 
 fn _how_many_words(f: &str) -> usize {
     let file = read_to_string(f)
@@ -17,13 +19,35 @@ fn _how_many_words(f: &str) -> usize {
     return map.len() as usize
 }
 
+struct ARngSeed([u8; N]);
+struct ARng(ARngSeed);
 
+impl Default for ARngSeed {
+    fn default() -> Self {
+        ARngSeed([0; N])
+    }
+}
+
+impl AsMut<[u8]> for ARngSeed{
+    fn as_mut(&mut self) -> &mut [u8] {
+        &mut self.0
+    }
+}
+
+impl SeedableRng for ARng {
+    type Seed = ARngSeed;
+    fn from_seed(seed: Self::Seed) -> Self {
+        ARng(seed)
+    }
+}
 
 fn get_rand_word() -> String{
     let words_n = _how_many_words(FILE);
+    let mut seed = ChaCha20Rng::from_entropy();
+    
     rand::thread_rng().gen_range(0..=words_n);
 
-    //rand::Rng::gen(&mut self)
+    
     
     let word_list: Vec<String> = read_to_string(FILE)
         .unwrap()
