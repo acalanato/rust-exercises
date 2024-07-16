7e85b6b37b4b84c66fda88d8911bedfef3493c1b
acalanato <vagner.assistencia@gmail.com>
2024-07-15 23:28:51 -0300

    Rand word query working

--- words/src/main.rs
+++ words/src/main.rs
@@ -1,10 +1,9 @@
 use std::collections::HashMap;
 use std::fs::read_to_string;
-use rand::{Rng, SeedableRng, prelude};
-
+use std::ops::Range;
+use rand::{Rng, SeedableRng};
 
 const FILE: &str = "file.txt";
-const N: usize = 64;
 
 fn _how_many_words(f: &str) -> usize {
     let file = read_to_string(f)
@@ -19,42 +18,16 @@ fn _how_many_words(f: &str) -> usize {
     return map.len() as usize
 }
 
-struct ARngSeed([u8; N]);
-struct ARng(ARngSeed);
-
-impl Default for ARngSeed {
-    fn default() -> Self {
-        ARngSeed([0; N])
-    }
-}
-
-impl AsMut<[u8]> for ARngSeed{
-    fn as_mut(&mut self) -> &mut [u8] {
-        &mut self.0
-    }
-}
-
-impl SeedableRng for ARng {
-    type Seed = ARngSeed;
-    fn from_seed(seed: Self::Seed) -> Self {
-        ARng(seed)
-    }
-}
 
 fn get_rand_word() -> String{
     let words_n = _how_many_words(FILE);
-    let mut seed = ChaCha20Rng::from_entropy();
-    
-    rand::thread_rng().gen_range(0..=words_n);
-
-    
-    
+    let choice = rand::thread_rng().gen_range(0..words_n);
     let word_list: Vec<String> = read_to_string(FILE)
         .unwrap()
         .lines()
         .map(String::from)
         .collect();
-    word_list[words_n].to_string()
+    word_list[choice].to_string()
 }
 
 fn main() {
