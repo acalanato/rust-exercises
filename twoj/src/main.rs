ebd7f47433871ffe9ffdbdcffcf63d6c0e14ad71
acalanato <vagner.assistencia@gmail.com>
2024-07-18 19:33:48 -0300

    Rinse and repeat

--- twoj/src/main.rs
+++ twoj/src/main.rs
@@ -1,3 +1,11 @@
+fn nbr_of_laps (x: u16, y: u16) -> (u16, u16) {
+    let _is_prime = |a: u16| if (a > 1) & (a < 4) {true;} else if a % 2 == 0 {false;};
+    
+    (x, y)
+}
+
+
+/*
 
 fn nbr_of_laps (x: u16, y: u16) -> (u16, u16) {
     let xv: Vec<u16> = (0..=u16::MAX).step_by(x as usize).collect();
@@ -9,47 +17,7 @@ fn nbr_of_laps (x: u16, y: u16) -> (u16, u16) {
     }
     (i / x, i / y)
 }
-/*
 
-fn nbr_of_laps (x: u16, y: u16) -> (u16, u16) {
-    let mut i = 1;
-    while x * i != y * i {
-        i += 1;
-        if i > 100 {break}
-    }
-    (i as u16, 0)
-}
-
-
-fn nbr_of_laps (x: u16, y: u16) -> (u16, u16) {
-    let mut mdc: u16 = 1;
-    let divide = |a, b| a / b;
-    while divide(x, mdc) != divide(y, mdc){
-        mdc += 1
-    }
-    (x.div(mdc), y.div(mdc))
-}
-
-
-fn nbr_of_laps (x: u16, y: u16) -> (u16, u16) {
-    let xv: Vec<u16> = (1..=50).step_by(x as usize).collect();
-    let yv: Vec<u16> = (1..=50).step_by(y as usize).collect();
-    
-    (xv[1],xv[3])
-}
-
-fn nbr_of_laps (x: u16, y: u16) -> (u16, u16) {
-    let s = |s| s + s;
-    let mut i = 0;
-    let mut out = (x, y);
-    loop {
-        out.0 = s(out.0);
-        out.1 = s(out.1);
-        i += 1;
-        if i > i {break};
-    }
-    return (out.0, out.1);
-}
 */
 fn main() {
     assert_eq!(nbr_of_laps(5, 3), (3, 5));
