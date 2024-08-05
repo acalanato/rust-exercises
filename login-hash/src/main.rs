674c09fac3e8311cac93de67d1730c311fc8347d
acalanato <vagner.assistencia@gmail.com>
2024-08-05 17:19:04 -0300

    Refactor

--- login-hash/src/main.rs
+++ login-hash/src/main.rs
@@ -1,4 +1,5 @@
-use std::io;
+use std::io::{self, Write, Result};
+use std::fs::File;
 use std::hash::{DefaultHasher, Hash, Hasher};
 #[allow(unused_variables)]
 
@@ -7,6 +8,22 @@ struct User {
     user: String,
     passwd: String,
 }
+
+/*
+fn create_user(us: String, pas: String) -> Result<()>{
+    let mut file = File::create("shadow")?;
+    let user = User {
+        user: us,
+        passwd: pas,
+    };
+    let mut u = DefaultHasher::new();
+    user.hash(&mut u);
+    let hash = u.finish();
+    //file.write_all(b"{hash}")?;
+    file.write(hash)?;
+    Ok(())
+}
+ */
 
 fn hash_p<T: Hash>(t: &T) -> u64{
     let mut p = DefaultHasher::new();
@@ -20,18 +37,18 @@ fn main() {
     
     'login: loop {
         'user: loop {
-            println!("User;");
+            println!("User");
             //let mut user = String::new();
             io::stdin()
                 .read_line(&mut user)
                 .expect("Invalid user");
-            let user: String = match user.trim().parse::<String>() {
+            let user = match user.trim().parse::<String>() {
                 Ok(username) => {username; break 'user},
                 Err(_) => continue,
             };
         }
         'pass: loop {
-            println!("Password;");
+            println!("Password:");
             io::stdin()
                 .read_line(&mut passwd)
                 .expect("Invalid user");
