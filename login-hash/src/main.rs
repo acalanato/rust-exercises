e7bac5d6b2ebc333430b2c27e812f3b9e6276929
acalanato <vagner.assistencia@gmail.com>
2024-08-06 11:32:18 -0300

    Fixed syntax, shadowing removed

--- login-hash/src/main.rs
+++ login-hash/src/main.rs
@@ -1,4 +1,4 @@
-use std::io::{self, Write, Result};
+use std::io::{self, Read, Result, Write};
 use std::fs::File;
 use std::hash::{DefaultHasher, Hash, Hasher};
 #[allow(unused_variables)]
@@ -37,13 +37,13 @@ fn main() {
     
     'login: loop {
         'user: loop {
-            println!("User");
+            println!("User:");
             //let mut user = String::new();
             io::stdin()
                 .read_line(&mut user)
                 .expect("Invalid user");
-            let user = match user.trim().parse::<String>() {
-                Ok(username) => {username; break 'user},
+            match user.trim().parse::<String>() {
+                Ok(username) => {drop(username); break 'user},
                 Err(_) => continue,
             };
         }
@@ -52,13 +52,13 @@ fn main() {
             io::stdin()
                 .read_line(&mut passwd)
                 .expect("Invalid user");
-            let passwd: String = match passwd.trim().parse::<String>() {
-                Ok(passwd) => {passwd; break 'pass},
+            match passwd.trim().parse::<String>() {
+                Ok(passwd) => {drop(passwd); break 'pass},
                 Err(_) => continue,
             };
         }
         break 'login;
     }
-    println!("Login =\t\t{}\nPassword =\t{}", user, passwd);
+    println!("\nLogin =\t\t{}Password =\t{}", user, passwd);
     println!("Sucess!")
 }
