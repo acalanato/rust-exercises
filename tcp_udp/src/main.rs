66960a2b2711606e764a48c4569cee8da7dd4988
acalanato <vagner.assistencia@gmail.com>
2024-08-20 11:51:12 -0300

    Refactor

--- tcp_udp/src/main.rs
+++ tcp_udp/src/main.rs
@@ -4,15 +4,15 @@ use std::{io::{Read, Result}, net::{TcpListener, TcpStream, UdpSocket}, env};
 const LOCALHOST: &str = "127.0.1.1";
 const PORT: &str = "789";
 
-fn send(mut stream: TcpStream) -> Result<()>{
-    stream = TcpStream::connect("10.0.1.1:80")?;
+fn _send(mut stream: TcpStream) -> Result<()>{
+    //stream = TcpStream::connect("10.0.1.1:80")?;
     let mut listen = String::new();
     stream.read_to_string(&mut listen)?;
     Ok(())
 }
 
 
-fn listen(stream: TcpListener)  {
+fn _listen(stream: TcpListener)  {
     println!("Listening on address {stream:?}");
     match stream.accept() {
 	Ok((_socket, addr)) => println!("funfou: {addr:?}"),
@@ -20,7 +20,7 @@ fn listen(stream: TcpListener)  {
     }
 }
 
-fn peer() -> Result<()>{
+fn _peer() -> Result<()>{
     let localhost = format!("{LOCALHOST}:{PORT}");
     let socket = UdpSocket::bind(localhost)?;
     socket.peer_addr()?;
@@ -32,7 +32,7 @@ fn peer() -> Result<()>{
 fn main() {
     let args: Vec<String> = env::args().collect();
     let localhost = format!("{LOCALHOST}:{PORT}");
-    let remote = peer();
+    let remote = _peer();
 /*
     match args {
 	send => {
