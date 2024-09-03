1e0b36e8a7cf759c14a44c87b16d54436760237b
acalanato <vagner.assistencia@gmail.com>
2024-09-03 19:49:47 -0300

    Done

---
+++ round_five/src/main.rs
@@ -0,0 +1,53 @@
+
+fn round_to_next_5(n: i32) -> i32 {
+    let mut out = n;
+    while out % 5 != 0 {
+	out += 1
+    }
+    out
+}
+
+fn main() {
+
+    println!("{}", round_to_next_5(3));
+    println!("{}", round_to_next_5(7));
+
+    println!("Sucess!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_basic() {
+        assert_eq!(round_to_next_5(1), 5);
+    }
+
+    #[test]
+    fn test_basic_neg() {
+        assert_eq!(round_to_next_5(-1), 0);
+    }
+
+    #[test]
+    fn test_extended() {
+        let tests = [
+            [0, 0],
+            [1, 5],
+            [-1, 0],
+            [-5, -5],
+            [3, 5],
+            [5, 5],
+            [7, 10],
+            [20, 20],
+            [39, 40],
+            [990, 990],
+            [121, 125],
+            [555, 555],
+        ];
+
+        for [x, out] in tests.iter() {
+            assert_eq!(round_to_next_5(*x), *out);
+        }
+    }
+}
