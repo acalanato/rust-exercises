d6d483ea568cc109ee1803f28b7333ecb5f0d083
acalanato <vagner.assistencia@gmail.com>
2024-09-06 18:40:17 -0300

    Tests

--- sum_serie/src/main.rs
+++ sum_serie/src/main.rs
@@ -1,13 +1,18 @@
 
 fn series_sum(n: u32) -> String {
 
-    let serie: Vec<_> = (0..n).map(|x| 1.0 * x as f32).collect();
+    //let serie: Vec<_> = (0..n).map(|x| 1.0 * x as f32).collect();
     
     let mut result = 0.0;
-
+    /*
     for x in 0..=n {
-	result =  result + (1.0 + (1.0 / (x as f32 * 1.0)) as f32)
+	result =  result + (1.0 * (1.0 * (x as f32 * 1.0)) as f32)
     }
+    */
+    //foo.iter().enumerate().map(|(i, val)| i + *val)
+
+    result = (0..=n).enumerate().map(|(i, val)| i as f32 + val as f32).last().expect("");
+    
     format!("{:.2}", result)
 }
 
