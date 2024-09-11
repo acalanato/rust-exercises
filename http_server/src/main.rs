1ee0875ba04a09da992ff5af27abc8b8d68aeaef
acalanato <vagner.assistencia@gmail.com>
2024-09-11 07:30:22 -0300

    Refactor

--- http_server/src/main.rs
+++ http_server/src/main.rs
@@ -3,7 +3,7 @@ use std::net::{TcpListener, TcpStream};
 use std::io::{prelude::*, BufReader};
 
 #[allow(unused)]
-fn handle_connection(mut stream: TcpStream) {
+fn _handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
 
     let http_request: Vec<_> = buf_reader
@@ -18,6 +18,25 @@ fn handle_connection(mut stream: TcpStream) {
 
     stream.write_all(response.as_bytes()).unwrap()
 }
+
+#[allow(unused)]
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = std::fs::read_to_string("SomeFile").unwrap();
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nCntent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap()
+}
+
 
 fn main() {
     println!("Logs from your program will appear here!");
