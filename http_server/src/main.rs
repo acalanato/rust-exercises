fc1bb2c888cc92b5a355c039cbf78ebeae0224c9
acalanato <vagner.assistencia@gmail.com>
2024-09-20 09:33:42 -0300

    Refactor

--- http_server/src/main.rs
+++ http_server/src/main.rs
@@ -1,23 +1,6 @@
 #[allow(unused_imports)]
 use std::net::{TcpListener, TcpStream};
-use std::io::{prelude::*, BufReader};
-
-#[allow(unused)]
-fn _handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
-    
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
-    
-    println!("Request: {http_request:#?}");
-
-    stream.write_all(response.as_bytes()).unwrap()
-}
+use std::io::{prelude::*, BufReader};    
 
 #[allow(unused)]
 fn handle_connection(mut stream: TcpStream) {
@@ -29,7 +12,7 @@ fn handle_connection(mut stream: TcpStream) {
         .collect();
 
     let status_line = "HTTP/1.1 200 OK";
-    let contents = std::fs::read_to_string("SomeFile").unwrap();
+    let contents = std::fs::read_to_string("Default.html").unwrap();
     let length = contents.len();
 
     let response = format!("{status_line}\r\nCntent-Length: {length}\r\n\r\n{contents}");
@@ -42,28 +25,8 @@ fn main() {
     println!("Logs from your program will appear here!");
     
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-    /*
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-	
-	handle_connection(stream);
-    }
-    
-    for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
-		for stream in listener.incoming() {
-		    let stream = stream.unwrap();
-		    println!("accepted new connection");
-		    handle_connection(stream)
-		}
-            }
-	    Err(e) => {
-                println!("error: {}", e);
-	    }
-	}
-    }
-     */
+    //let listener = TcpListener::bind("192.168.0.109:4221").unwrap();
+
     for stream in listener.incoming() {
 	match stream {
 	    Ok(stream) => {handle_connection(stream)},
