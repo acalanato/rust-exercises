2e7c09311dca33915592ef6797926628dbde7159
acalanato <vagner.assistencia@gmail.com>
2024-09-24 10:13:10 -0300

    Done

--- reverse_string/src/main.rs
+++ reverse_string/src/main.rs
@@ -3,6 +3,9 @@ fn solution(phrase: &str) -> String {
      String::from_utf8(phrase.bytes().into_iter().rev().collect()).unwrap()
 }
 
+fn _solution(phrase: &str) -> String {
+    return phrase.chars().rev().collect::<String>();
+}
 
 fn main() {
     println!("Hello, world!");
